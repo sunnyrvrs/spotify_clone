1e2ff7693b8e424fb217d0690baed7ffeaeb1139
Sunny Rivers <sunnyrvrs@gmail.com>
2022-01-19 13:36:14 -0800

    fixed provider code

--- pages/login.tsx
+++ pages/login.tsx
@@ -6,11 +6,11 @@ function login({ providers }) {
             <img className="w-52 mb-5" src="https://links.papareact.com/9xl" alt=""/>
 
             {Object.values(providers).map((provider) => (
-                <div key={provider.name}>
+                <div key={provider['name']}>
                     <button 
                         className="bg-[#18D860] text-white p-5 rounded-full" 
-                        onClick={() => signIn(provider.id, { callbackUrl: '/' })}
-                    >Login with {provider.name}</button>
+                        onClick={() => signIn(provider['id'], { callbackUrl: '/' })}
+                    >Login with {provider['name']}</button>
                 </div>
             ))}
         </div>
